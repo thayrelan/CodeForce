11534f67a7a0d7d55badfc411d31055f8a3ceca0
Lais Menini <laismenini@MacBook-Pro-de-Lais.local>
2022-12-16 02:03:35 -0300

    Versao Organizada Football.java

--- lessThousand/FootBall.java
+++ lessThousand/FootBall.java
@@ -8,60 +8,89 @@ public class FootBall{
         //Input
         int n, a, b;
         //Output
-        int d;
+        int drawNumber;
         int[][] score;
-
+        
         n = readVar.nextInt();
         a = readVar.nextInt();
         b = readVar.nextInt();
 
-        d = minimumDraws(n, a, b);
-        if(d < 0){
-            System.out.println("0");
-        }
-        else{
-            System.out.println(d);
-        }
+
+        drawNumber = minimumDraws(n, a, b);
+        System.out.println(drawNumber);
         
-        if(n == 1 && a == b){
+        if(n == 1){
             System.out.println(a + ":" + b);
         }
         else{
-            score = scoreMaker(a, b, n, d);
+            score = scoreMaker(a, b, n, drawNumber);
             for (int i = 0; i < n; i++) {
                 System.out.println(score[0][i] + ":" + score[1][i]);
             }
         }
-
+ 
     }
-
+ 
     public static int minimumDraws(int n, int a, int b){
-        int d;
-        boolean totalGoals = a + b < n ? true : false;
+        int drawNumber = (n - (a+b));
+        boolean isPossible = a + b >= n ? true : false;
         if ((n == 1 && a == b)) {
             return 1;
         }
-        if (!totalGoals && n > 1){
+        if (isPossible && n > 1 || drawNumber < 0){
             return 0;
         }
-        return (n - (a+b));
+        return drawNumber;
     }
 
-    public static int[][] scoreMaker(int a, int b, int n, int d){
-        int [][] score = new int[2][n];
-        int i =0;
-        int factor =0;
-        while (a != 0) {
-            score[0][i] += 1;
-            a--;
-            if(i+1 == n){
+    public static int scoreFiller(int[]score, int goalScored){
+        
+        int i=0;
+        while (goalScored != 0) {
+            score[i] += 1;
+            goalScored--;
+            if(i+1 == score.length){
                 i = 0;
             }
             else{
                 i++;
             }
         }
-        i =0;
+        return goalScored;
+    }
+    public static boolean scoreRearranger(int[][] score, int i, boolean changeIt){
+
+        if(i+1 == score[0].length){
+            if(score[0][i] == score[1][i]){
+                score[0][i]--;
+                score[0][0]++;
+            }
+            if(score[0][0] == score[1][0]){
+                score[1][0]--;
+                score[1][i]++;
+            }
+            return changeIt;
+        }
+        if(score[0][i] == score[1][i]){
+            score[0][i]--;
+            score[0][i+1]++;
+            changeIt = true;
+        }
+        if(score[0][i+1] == score[1][i+1] && changeIt == true){
+            score[1][i+1]--;
+            score[1][i]++;
+        }
+        changeIt = false;
+        return changeIt;
+    }
+    public static int[][] scoreMaker(int a, int b, int n, int drawNumber){
+        int [][] score = new int[2][n];
+        int i=0;
+        boolean changeIt;
+        
+        a = scoreFiller(score[0], a);
+
+        //Fills the score of the opponent team starting from the unfilled scores of a
         while(b != 0){
             while(score[0][i] == score[1][i]){
                 score[1][i] += 1;
@@ -74,41 +103,12 @@ public class FootBall{
                 i++;
             }
         }
-        i=0;
-        while(b != 0){
-            score[1][i] += 1;
-            b--;
-            if(i+1 == n){
-                i = 0;
-            }
-            else{
-                i++;
-            }
-        }
         
-        if(d == 0 && n != 1){
-            for (int j = 0; j < n; j++) {
-                if(j+1 == n){
-                    if(score[0][j] == score[1][j]){
-                        score[0][j]--;
-                        score[0][0]++;
-                    }
-                    if(score[0][0] == score[1][0]){
-                        score[1][0]--;
-                        score[1][j]++;
-                    }
-                    break;
-                }
-                if(score[0][j] == score[1][j]){
-                    score[0][j]--;
-                    score[0][j+1]++;
-                    factor = 1;
-                }
-                if(score[0][j+1] == score[1][j+1] && factor == 1){
-                    score[1][j+1]--;
-                    score[1][j]++;
-                }
-                factor = 0;
+        b = scoreFiller(score[1], b);
+        
+        if(drawNumber == 0){
+            for (i = 0; i < n; i++) {
+                changeIt = scoreRearranger(score, i, false);
             }
         }
         return score;
